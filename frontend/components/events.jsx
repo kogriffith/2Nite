b9d305b0ab41f992e08c81e6cc7ab59cab7460a3
gryyph <kevin.o.griffith@gmail.com>
2020-01-11 22:17:01 -0800

    events and package-lock

--- frontend/components/events.jsx
+++ frontend/components/events.jsx
@@ -7,6 +7,7 @@ const Events = () => {
     const [coordinates, setCoordinates] = React.useState(["0", "0"]);
 
     const getNearbyEvents = () => {
+      
         window.navigator.geolocation.getCurrentPosition((georesponse) => {
           setCoordinates([georesponse.coords.latitude, georesponse.coords.longitude]);
           // console.log(coordinates);
@@ -14,13 +15,14 @@ const Events = () => {
           const seat_geek_secret = "840dee17d5527ac416769abaa4ce351eb9abc856052508cea5dc45abc859dfcf";
       
           const url = "https://api.seatgeek.com/2/events?client_id=" + seat_geek_id + "&client_secret=" + seat_geek_secret
-            + "&lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&range=30mi" + "&sort=score.desc";
+            + "&lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&range=30mi" /*+ "&sort=score.desc"*/;
           axios.get(url)
             .then((res) => {
               setNearbyEvents(JSON.stringify(res));
             })
             .catch((e) => console.log(e));
         });
+        console.log(nearbyEvents);
       };
       
 
